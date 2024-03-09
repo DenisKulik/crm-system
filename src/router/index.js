ba03e9bad4bfe7ac079d6dfa702cbd7ca89b9dbd
Denis Kulik <kulikdenis1996x@gmail.com>
2024-03-09 18:52:51 +0700

    feat: add routes for login and categories pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import HomeView from '@/views/HomeView.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +9,16 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/LoginView.vue'),
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    component: () => import('@/views/CategoriesView.vue'),
   },
 ];
 
