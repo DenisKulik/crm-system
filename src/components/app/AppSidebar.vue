76740b33233b01cc2043146533c9543aecc37fbf
Denis Kulik <kulikdenis1996x@gmail.com>
2024-03-16 15:48:30 +0700

    feat: - add AppSidebar and AppNavbar components - add links to go to application pages

---
+++ src/components/app/AppSidebar.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul class="sidenav app-sidenav open">
+    <RouterLink
+      v-for="link in links"
+      :key="link.url"
+      :to="link.url"
+      :exact="link.exact"
+      tag="li"
+      active-class="active"
+    >
+      <a href="#" class="waves-effect waves-orange pointer">{{ link.name }}</a>
+    </RouterLink>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'AppSidebar',
+  data: () => ({
+    links: [
+      {
+        name: 'Счет',
+        url: '/',
+        exact: true,
+      },
+      {
+        name: 'История',
+        url: '/history',
+      },
+      {
+        name: 'Планирование',
+        url: '/planning',
+      },
+      {
+        name: 'Новая запись',
+        url: '/record',
+      },
+      {
+        name: 'Категории',
+        url: '/categories',
+      },
+    ],
+  }),
+};
+</script>
