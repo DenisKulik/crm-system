068fcc18ceee9346505023851e9845ac8d0e1185
Denis Kulik <kulikdenis1996x@gmail.com>
2024-04-15 16:51:57 +0700

    feat: add login localization

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,29 +1,26 @@
 <template>
   <form class="card auth-card" @submit.prevent="onSubmit">
     <div class="card-content">
-      <span class="card-title">Домашняя бухгалтерия</span>
+      <span class="card-title">{{ 'CRM_Title' | localize }}</span>
       <div class="input-field">
         <input
           id="email"
           type="text"
           v-model.trim="email"
-          :class="{
-            invalid: ($v.email.$dirty && !$v.email.required)
-            || ($v.email.$dirty && !$v.email.email)
-          }"
+          :class="{invalid: $v.email.$error}"
         >
         <label for="email">Email</label>
         <small
           v-if="$v.email.$dirty && !$v.email.required"
           class="helper-text invalid"
         >
-          Поле Email не должно быть пустым
+          {{ 'Message_EmailEmpty' | localize }}
         </small>
         <small
           v-else-if="$v.email.$dirty && !$v.email.email"
           class="helper-text invalid"
         >
-          Введите корректный Email
+          {{ 'Message_EmailIncorrect' | localize }}
         </small>
       </div>
       <div class="input-field">
@@ -31,23 +28,21 @@
           id="password"
           type="password"
           v-model.trim="password"
-          :class="{
-            invalid: ($v.password.$dirty && !$v.password.required)
-            || ($v.password.$dirty && !$v.password.minLength)
-          }"
+          :class="{invalid: $v.password.$error}"
         >
-        <label for="password">Пароль</label>
+        <label for="password">{{ 'Password' | localize }}</label>
         <small
           v-if="$v.password.$dirty && !$v.password.required"
           class="helper-text invalid"
         >
-          Введите пароль
+          {{ 'Message_EnterPassword' | localize }}
         </small>
         <small
           v-else-if="$v.password.$dirty && !$v.password.minLength"
           class="helper-text invalid"
         >
-          Пароль должен быть длиннее {{ $v.password.$params.minLength.min }} символов
+          {{ 'Password' | localize }} {{ 'Message_MustBeLonger' | localize }} {{
+            $v.password.$params.minLength.min }} {{ 'Message_Characters' | localize }}
         </small>
       </div>
     </div>
@@ -57,14 +52,14 @@
           class="btn waves-effect waves-light auth-submit"
           type="submit"
         >
-          Войти
+          {{ 'Enter' | localize }}
           <i class="material-icons right">send</i>
         </button>
       </div>
 
       <p class="center">
         Нет аккаунта?
-        <RouterLink to="/register">Зарегистрироваться</RouterLink>
+        <RouterLink to="/register">{{ 'Register' | localize }}</RouterLink>
       </p>
     </div>
   </form>
