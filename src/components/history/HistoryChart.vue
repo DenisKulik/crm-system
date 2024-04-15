624525808c3476084fae21d6bdeeff17a650b2b9
Denis Kulik <kulikdenis1996x@gmail.com>
2024-04-15 12:06:21 +0700

    feat: add history chart

---
+++ src/components/history/HistoryChart.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="history-chart">
+    <PieChart
+      :data="chartData"
+      :options="chartOptions"
+      :style="{ height: '320px', width: '320px' }"
+    />
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Pie as PieChart } from 'vue-chartjs';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale,
+} from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
+
+export default {
+  name: 'HistoryChart',
+  components: {
+    PieChart,
+  },
+  props: {
+    records: {
+      type: Array,
+      default: () => [],
+    },
+    categories: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data: () => ({
+    chartOptions: {
+      responsive: true,
+    },
+  }),
+  computed: {
+    chartData() {
+      if (!this.records.length || !this.categories.length) {
+        return {
+          labels: [],
+          datasets: [],
+        };
+      }
+
+      return {
+        labels: this.categories.map((category) => category.title),
+        datasets: [
+          {
+            label: 'Расходы по категории',
+            data: this.categories.map((category) => this.records.reduce((total, record) => {
+              if (record.categoryID === category.id && record.type === 'outcome') {
+                total += Number(record.amount);
+              }
+              return total;
+            }, 0)),
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      };
+    },
+  },
+};
+</script>
--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -3,11 +3,10 @@
     <div class="page-title">
       <h3>История записей</h3>
     </div>
-
-    <div class="history-chart">
-      <canvas></canvas>
-    </div>
-
+    <HistoryChart
+      :records="records"
+      :categories="categories"
+    />
     <AppLoader v-if="loading"/>
     <p class="center" v-else-if="!records.length">
       Записей пока нет.
@@ -15,7 +14,6 @@
     </p>
     <section v-else>
       <HistoryTable :records="items"/>
-
       <AppPaginate
         v-model="currentPage"
         :page-count="pageCount"
@@ -30,21 +28,28 @@
 </template>
 
 <script>
+// mixins
 import { paginationMixin } from '@/mixins';
+// components
 import HistoryTable from '@/components/history/HistoryTable.vue';
+import HistoryChart from '@/components/history/HistoryChart.vue';
 
 export default {
   name: 'HistoryView',
-  components: { HistoryTable },
+  components: {
+    HistoryChart,
+    HistoryTable,
+  },
   mixins: [paginationMixin],
   data: () => ({
     loading: true,
     records: [],
+    categories: [],
   }),
   async mounted() {
     this.records = await this.$store.dispatch('fetchRecords');
-    const categories = await this.$store.dispatch('fetchCategories');
-    this.setupPagination(this.formatRecords(this.records, categories));
+    this.categories = await this.$store.dispatch('fetchCategories');
+    this.setupPagination(this.formatRecords(this.records, this.categories));
     this.loading = false;
   },
   methods: {
